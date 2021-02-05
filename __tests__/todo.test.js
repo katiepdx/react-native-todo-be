6a1099a67db36e2855cb1b9086fce74bfceceec1
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-02-05 10:08:08 -0800

    add data helpers and getById test

--- __tests__/todo.test.js
+++ __tests__/todo.test.js
@@ -1,14 +1,9 @@
-const fs = require('fs');
-const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+require('../lib/data/data-helpers');
 
 describe('react-native-todo-be routes', () => {
-  // beforeEach(() => {
-  //   return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
-  // });
-
-  it('should create a todo for a user using the POST method', async () => {
+  it('should create a todo for a user using POST', async () => {
     return await request(app)
       .post('/api/v1/todos')
       .send({
@@ -34,7 +29,7 @@ describe('react-native-todo-be routes', () => {
       });
   });
 
-  it('should get all todos in the database using the GET method', async () => {
+  it('should get all todos in the database GET', async () => {
     await request(app)
       .get('/api/v1/todos')
       .then(res => {
@@ -48,6 +43,23 @@ describe('react-native-todo-be routes', () => {
           dateAdded: expect.any(String),
           dateCompleted: expect.any(String)
         }]));
+      });
+  });
+
+  it('should get a todo by id from the database using GET', async () => {
+    await request(app)
+      .get('/api/v1/todos/1')
+      .then(res => {
+        expect(res.body).toEqual({
+          todoId: '1',
+          userEmail: expect.any(String),
+          todo: expect.any(String),
+          notes: expect.any(String),
+          completed: expect.any(Boolean),
+          tags: expect.any(Array),
+          dateAdded: expect.any(String),
+          dateCompleted: expect.any(String)
+        });
       });
   });
 });
