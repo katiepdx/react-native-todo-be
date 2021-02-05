63f422246c84a2b22ab4eaafa14be95a8d2fc94b
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-02-04 21:55:07 -0800

    add GET route and test - getAll

--- __tests__/todo.test.js
+++ __tests__/todo.test.js
@@ -4,9 +4,9 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('react-native-todo-be routes', () => {
-  beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
-  });
+  // beforeEach(() => {
+  //   return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  // });
 
   it('should create a todo for a user using the POST method', async () => {
     return await request(app)
@@ -31,6 +31,23 @@ describe('react-native-todo-be routes', () => {
           dateAdded: expect.any(String),
           dateCompleted: expect.any(String)
         });
+      });
+  });
+
+  it('should get all todos in the database using the GET method', async () => {
+    await request(app)
+      .get('/api/v1/todos')
+      .then(res => {
+        expect(res.body).toEqual(expect.arrayContaining([{
+          todoId: expect.any(String),
+          userEmail: expect.any(String),
+          todo: expect.any(String),
+          notes: expect.any(String),
+          completed: expect.any(Boolean),
+          tags: expect.any(Array),
+          dateAdded: expect.any(String),
+          dateCompleted: expect.any(String)
+        }]));
       });
   });
 });
